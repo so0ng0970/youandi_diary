a9eda143f80d5e21b85f8837d7152f467c929139
soonger3306 <soonger3306@daum.net>
2023-03-22 16:25:08 +0900

    first

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   Timer(const Duration(seconds: 2), () {
+  //     Navigator.push(
+  //       context,
+  //       MaterialPageRoute(
+  //         builder: (context) {
+  //           return const LoginPage();
+  //         },
+  //       ),
+  //     );
+  //   });
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('image/bg.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
+  }
+}
