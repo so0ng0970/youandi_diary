5876f430b152bff44cd66380e5a9161f0f940da1
soonger3306 <soonger3306@daum.net>
2023-03-22 20:06:46 +0900

    login,sign

--- lib/screens/sign_page.dart
+++ lib/screens/sign_page.dart
@@ -1,32 +1,49 @@
 import 'package:flutter/material.dart';
-import 'package:youandi_diary/screens/sign_page.dart';
+import 'package:youandi_diary/screens/login_page.dart';
 
-class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
+class SignScreen extends StatefulWidget {
+  const SignScreen({super.key});
 
   @override
-  State<LoginPage> createState() => _LoginPageState();
+  State<SignScreen> createState() => _SignScreenState();
 }
 
-class _LoginPageState extends State<LoginPage> {
-  final _formKey = GlobalKey<FormState>();
-  bool isLoginScreen = true;
+class _SignScreenState extends State<SignScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 30,
+        backgroundColor: Colors.blue[100],
+        elevation: 0.0,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          iconSize: 20,
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) {
+                  return const LoginPage();
+                },
+              ),
+            );
+          },
+        ),
+      ),
       backgroundColor: Colors.blue[100],
       body: Column(
         children: [
           Container(
-            margin: const EdgeInsets.fromLTRB(20, 50, 20, 0),
+            margin: const EdgeInsets.fromLTRB(20, 0, 20, 0),
             decoration: BoxDecoration(
               color: Colors.blue[200],
             ),
             child: Column(
-              children: [
+              children: const [
                 Text(
-                  isLoginScreen ? 'Login' : 'sign',
-                  style: const TextStyle(
+                  'sign',
+                  style: TextStyle(
                     fontSize: 40,
                     fontWeight: FontWeight.bold,
                     color: Colors.white,
@@ -44,7 +61,6 @@ class _LoginPageState extends State<LoginPage> {
                 color: Colors.blue[200],
               ),
               child: Form(
-                key: _formKey,
                 child: Column(
                   children: [
                     TextFormField(
@@ -113,21 +129,6 @@ class _LoginPageState extends State<LoginPage> {
                             Radius.circular(20.0),
                           ),
                         ),
-                      ),
-                    ),
-                    TextButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) {
-                              return const SignScreen();
-                            },
-                          ),
-                        );
-                      },
-                      child: const Text(
-                        '회원이 아니신가요?',
                       ),
                     ),
                     IconButton(onPressed: () {}, icon: const Icon(Icons.check))
