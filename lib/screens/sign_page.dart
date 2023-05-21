c3549ca4b56511e542d5e799732045866f6b0b30
soonger3306 <soonger3306@daum.net>
2023-05-21 19:23:14 +0900

    ui

--- lib/screens/sign_page.dart
+++ lib/screens/sign_page.dart
@@ -38,195 +38,198 @@ class _SignScreenState extends State<SignScreen> {
         ),
       ),
       backgroundColor: Colors.blue[100],
-      body: Column(
-        children: [
-          Container(
-            margin: const EdgeInsets.fromLTRB(20, 0, 20, 0),
-            decoration: BoxDecoration(
-              color: Colors.blue[200],
-            ),
-            child: Column(
-              children: const [
-                Text(
-                  'sign',
-                  style: TextStyle(
-                    fontSize: 40,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Center(
-            child: Container(
-              margin: const EdgeInsets.only(top: 80),
-              height: MediaQuery.of(context).size.height - 280,
-              width: MediaQuery.of(context).size.width - 80,
+      body: SingleChildScrollView(
+        keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+        child: Column(
+          children: [
+            Container(
+              margin: const EdgeInsets.fromLTRB(20, 0, 20, 0),
               decoration: BoxDecoration(
                 color: Colors.blue[200],
               ),
-              child: Form(
-                key: formKey,
-                child: Column(
-                  children: [
-                    TextFormField(
-                      key: const ValueKey(1),
-                      focusNode: _nicknameFocus,
-                      validator: (value) => CheckValidate()
-                          .validateNickName(_nicknameFocus, value!),
-                      keyboardType: TextInputType.name,
-                      onSaved: (value) {
-                        userName = value!;
-                      },
-                      onChanged: (value) {
-                        userName = value;
-                      },
-                      decoration: const InputDecoration(
-                        hintText: '닉네임',
-                        enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.grey),
-                          borderRadius: BorderRadius.all(
-                            Radius.circular(20.0),
-                          ),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.grey),
-                          borderRadius: BorderRadius.all(
-                            Radius.circular(20.0),
-                          ),
-                        ),
-                      ),
+              child: Column(
+                children: const [
+                  Text(
+                    'sign',
+                    style: TextStyle(
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
                     ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    TextFormField(
-                      key: const ValueKey(2),
-                      focusNode: _emailFocus,
-                      validator: (value) =>
-                          CheckValidate().validateEmail(_emailFocus, value!),
-                      keyboardType: TextInputType.emailAddress,
-                      onSaved: (newValue) {
-                        userEmail = newValue!;
-                      },
-                      onChanged: (value) {
-                        userEmail = value;
-                      },
-                      decoration: const InputDecoration(
-                        hintText: '이메일주소',
-                        enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.grey),
-                          borderRadius: BorderRadius.all(
-                            Radius.circular(20.0),
-                          ),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.grey),
-                          borderRadius: BorderRadius.all(
-                            Radius.circular(20.0),
-                          ),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    TextFormField(
-                      key: const ValueKey(3),
-                      focusNode: _passwordFocus,
-                      validator: (value) => CheckValidate()
-                          .validatePassword(_passwordFocus, value!),
-                      keyboardType: TextInputType.name,
-                      onSaved: (value) {
-                        userPassword = value!;
-                      },
-                      onChanged: (value) {
-                        userPassword = value;
-                      },
-                      decoration: const InputDecoration(
-                        hintText: '비밀번호',
-                        enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.grey),
-                          borderRadius: BorderRadius.all(
-                            Radius.circular(20.0),
-                          ),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.grey),
-                          borderRadius: BorderRadius.all(
-                            Radius.circular(20.0),
-                          ),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    TextFormField(
-                      decoration: const InputDecoration(
-                        hintText: '비밀번호(확인)',
-                        enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.grey),
-                          borderRadius: BorderRadius.all(
-                            Radius.circular(20.0),
-                          ),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.grey),
-                          borderRadius: BorderRadius.all(
-                            Radius.circular(20.0),
-                          ),
-                        ),
-                      ),
-                    ),
-                    IconButton(
-                        onPressed: () async {
-                          formKey.currentState?.validate();
-                          try {
-                            final newUser = await _authentication
-                                .createUserWithEmailAndPassword(
-                              email: userEmail,
-                              password: userPassword,
-                            );
-                            await FirebaseFirestore.instance
-                                .collection('user')
-                                .doc(newUser.user!.uid)
-                                .set({
-                              'userName': userEmail,
-                              'email': userEmail,
-                            });
-                            if (newUser.user != null) {
-                              // ignore: use_build_context_synchronously
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) {
-                                    return const LoginPage();
-                                  },
-                                ),
-                              );
-                            }
-                          } catch (e) {
-                            print(e);
-                            if (mounted) {
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                const SnackBar(
-                                  content: Text(
-                                      'Please check your email and password'),
-                                  backgroundColor: Colors.green,
-                                ),
-                              );
-                            }
-                          }
+                  ),
+                ],
+              ),
+            ),
+            Center(
+              child: Container(
+                margin: const EdgeInsets.only(top: 80),
+                height: MediaQuery.of(context).size.height - 280,
+                width: MediaQuery.of(context).size.width - 80,
+                decoration: BoxDecoration(
+                  color: Colors.blue[200],
+                ),
+                child: Form(
+                  key: formKey,
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        key: const ValueKey(1),
+                        focusNode: _nicknameFocus,
+                        validator: (value) => CheckValidate()
+                            .validateNickName(_nicknameFocus, value!),
+                        keyboardType: TextInputType.name,
+                        onSaved: (value) {
+                          userName = value!;
                         },
-                        icon: const Icon(Icons.check))
-                  ],
+                        onChanged: (value) {
+                          userName = value;
+                        },
+                        decoration: const InputDecoration(
+                          hintText: '닉네임',
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.grey),
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(20.0),
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.grey),
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(20.0),
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      TextFormField(
+                        key: const ValueKey(2),
+                        focusNode: _emailFocus,
+                        validator: (value) =>
+                            CheckValidate().validateEmail(_emailFocus, value!),
+                        keyboardType: TextInputType.emailAddress,
+                        onSaved: (newValue) {
+                          userEmail = newValue!;
+                        },
+                        onChanged: (value) {
+                          userEmail = value;
+                        },
+                        decoration: const InputDecoration(
+                          hintText: '이메일주소',
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.grey),
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(20.0),
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.grey),
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(20.0),
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      TextFormField(
+                        key: const ValueKey(3),
+                        focusNode: _passwordFocus,
+                        validator: (value) => CheckValidate()
+                            .validatePassword(_passwordFocus, value!),
+                        keyboardType: TextInputType.name,
+                        onSaved: (value) {
+                          userPassword = value!;
+                        },
+                        onChanged: (value) {
+                          userPassword = value;
+                        },
+                        decoration: const InputDecoration(
+                          hintText: '비밀번호',
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.grey),
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(20.0),
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.grey),
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(20.0),
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          hintText: '비밀번호(확인)',
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.grey),
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(20.0),
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.grey),
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(20.0),
+                            ),
+                          ),
+                        ),
+                      ),
+                      IconButton(
+                          onPressed: () async {
+                            formKey.currentState?.validate();
+                            try {
+                              final newUser = await _authentication
+                                  .createUserWithEmailAndPassword(
+                                email: userEmail,
+                                password: userPassword,
+                              );
+                              await FirebaseFirestore.instance
+                                  .collection('user')
+                                  .doc(newUser.user!.uid)
+                                  .set({
+                                'userName': userEmail,
+                                'email': userEmail,
+                              });
+                              if (newUser.user != null) {
+                                // ignore: use_build_context_synchronously
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) {
+                                      return const LoginPage();
+                                    },
+                                  ),
+                                );
+                              }
+                            } catch (e) {
+                              print(e);
+                              if (mounted) {
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  const SnackBar(
+                                    content: Text(
+                                        'Please check your email and password'),
+                                    backgroundColor: Colors.green,
+                                  ),
+                                );
+                              }
+                            }
+                          },
+                          icon: const Icon(Icons.check))
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
