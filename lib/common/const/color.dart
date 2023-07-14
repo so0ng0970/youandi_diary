b0c86307c9136d9c99f4e89076dc650e2c7a0c0f
soonger3306 <soonger3306@daum.net>
2023-07-14 23:49:21 +0900

    feat:homescreen ui

--- lib/common/const/color.dart
+++ lib/common/const/color.dart
@@ -13,3 +13,6 @@ const SIGN_TEXT_COLOR = Color(0xFF105696);
 
 const UNDERLINE_INPUT_COLOR = Color(0xFF0d66b7);
 const UNDERLINE_FOCUS_COLOR = Color(0xFF0d66b7);
+
+// homeSreen
+const HOMESCREEN_COLOR = Color(0xFF7cacd8);
--- lib/common/screen/home_screen.dart
+++ lib/common/screen/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:youandi_diary/common/const/color.dart';
 import 'package:youandi_diary/user/model/kakao_login.dart';
 import 'package:youandi_diary/user/model/social_view_model.dart';
 
@@ -14,37 +15,50 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: HOMESCREEN_COLOR,
+      // drawer: MainDrawer(
+      //   profileImg: profileImg,
+      //   nickName: nickName,
+      //   email: email,
+      // ),
       body: CustomScrollView(
         slivers: [
           SliverAppBar(
-            expandedHeight: 200,
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(
+                  50,
+                ),
+                bottomRight: Radius.circular(
+                  50,
+                ),
+              ), //모서리
+            ), //테두리
+            backgroundColor: BACKGROUND_COLOR,
+            expandedHeight: 250,
             centerTitle: true,
             flexibleSpace: FlexibleSpaceBar(
-              background: SafeArea(
-                child: Image.asset(
-                  'asset/image/cloud.gif',
-                ),
+              background: Image.asset(
+                'asset/image/cloud.gif',
+                scale: 2.0,
+              ),
+              title: const Text('Let\'s keep a record !'),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Container(
+              child: ListView(
+                shrinkWrap: true, // 스크롤 가능한 영역만 차지하도록 설정
+                physics: const NeverScrollableScrollPhysics(), // 스크롤 비활성화
+                children: [
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text(''),
+                  ),
+                ],
               ),
             ),
           ),
-          // SliverToBoxAdapter(
-          //   child: DefaultLayout(
-          //     appBar: null, // 빈 appBar 사용
-          //     child: Container(
-          //       color: BACKGROUND_COLOR,
-          //       child: ListView(
-          //         shrinkWrap: true, // 스크롤 가능한 영역만 차지하도록 설정
-          //         physics: const NeverScrollableScrollPhysics(), // 스크롤 비활성화
-          //         children: [
-          //           OutlinedButton(
-          //             onPressed: () {},
-          //             child: const Text(''),
-          //           ),
-          //         ],
-          //       ),
-          //     ),
-          //   ),
-          // ),
         ],
       ),
     );
